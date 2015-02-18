4e6c3dc6a21fa970cb8d6211a78e326737418773
Igor Balanici <igor.balanici@gmail.com>
2015-02-19 00:46:55 +0200

    Added templates for block and page. Added hooks for blocks, theme, menu.

---
+++ templates/block_typical.tpl.php
@@ -0,0 +1,8 @@
+<?php
+//$Id$
+/**
+ * @file typical.tpl.php
+ * Template for typical(most viewed) exchange rates
+ */
+?>
+    this is a bnm-rates-block-typical block
---
+++ templates/page_typical.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 2/19/15
+ * Time: 12:00 AM
+ */
+?>
+page_typical
