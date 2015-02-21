7931e79bb1694e11398eceb554b14c15968f7095
Igor Balanici <igor.balanici@gmail.com>
2015-02-22 00:31:37 +0200

    function for load xml from bnm url and from file for development

---
+++ include/bnm_rates_functions.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @param $arg
+ * @return mixed
+ */
+function bnm_rates_probe($arg) {
+    return $arg;
+}
+
+function bnm_rates_get_xmldata($date = 'rewuired', $lang = 'en') {
+    //link http://www.bnm.md/md/official_exchange_rates?get_xml=1&date=20.01.2015
+    //just for today:
+    $date = '22.01.2015';
+    $url = "http://www.bnm.md/{$lang}/official_exchange_rates?get_xml=1&date={$date}";
+    $url = drupal_get_path('module', 'bnm_rates') . '/official_exchange_rates.xml';
+    $xml = simplexml_load_file($url);
+    return $xml;
+}
