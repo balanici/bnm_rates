372daeda7e5029492b59cf315397682dd0a6abc0
Igor Balanici <igor.balanici@gmail.com>
2015-02-24 08:23:51 +0200

    drupal code style reformatted

--- include/bnm_rates_functions.inc.php
+++ include/bnm_rates_functions.inc.php
@@ -5,7 +5,7 @@
  * @return mixed
  */
 function bnm_rates_probe($arg) {
-    return $arg;
+  return $arg;
 }
 
 /**
@@ -17,67 +17,76 @@ function bnm_rates_probe($arg) {
  *
  * @author idoctor
  */
-function bnm_rates_pull_xmldata($date = '', $lang = 'ru') {
-    //link http://www.bnm.md/md/official_exchange_rates?get_xml=1&date=20.01.2015
-    //just for today if empty
-    if (empty($date)) {
-        $date = date("d.m.Y");
-    }
-    print_r($date);
-    $url = "http://www.bnm.md/{$lang}/official_exchange_rates?get_xml=1&date={$date}";
+function bnm_rates_pull_xmldata($date = '', $lang = 'en') {
+  //link http://www.bnm.md/md/official_exchange_rates?get_xml=1&date=20.01.2015
+  //just for today if empty
+  if (empty($date)) {
+    $date = date("d.m.Y");
+  }
+  print_r($date);
+  $url = "http://www.bnm.md/{$lang}/official_exchange_rates?get_xml=1&date={$date}";
 
-    //for dev
+  //for dev
 //    $url = drupal_get_path('module', 'bnm_rates') . '/official_exchange_rates.xml';
 
-    $simple_xml = simplexml_load_file($url);
-    if ($simple_xml) {
-        $result = bnm_rates_store_data($simple_xml, $lang);
-        watchdog('bnm_rates', "Store rates for {$date} lang={$lang}. Result: {$result}");
-    } else {
-        watchdog('bnm_rates', "SimpleXML NULL or FALSE for {$date} lang={$lang}.");
-    }
+  $simple_xml = simplexml_load_file($url);
+  if ($simple_xml) {
+    $result = bnm_rates_store_data($simple_xml, $lang);
+    watchdog('bnm_rates', "Store rates for {$date} lang={$lang}. Result: {$result}");
+  }
+  else {
+    watchdog('bnm_rates', "SimpleXML NULL or FALSE for {$date} lang={$lang}.");
+  }
 
-    return $simple_xml;
+  return $simple_xml;
 }
 
 /**
  * Stores xmldata into database
  */
 function bnm_rates_store_data($simple_xml, $lang) {
-    $attribs = $simple_xml->attributes();
-    $valute_array = $simple_xml->children();
-    var_dump($attribs);
-    foreach ($valute_array as $valute) {
-        $attr = $valute->attributes();
-        $valute_id = (string)$attr['ID'];
-        $currency_record = db_merge('bnm_currency')
-            ->key(array(
-                'valute_id' => (int) $valute_id,
-                'lang' => $lang,
-            ))
-            ->fields(
-                array(
-                    'num_code' => (string) $valute->NumCode,
-                    'char_code' => (string) $valute->CharCode,
-                    'nominal' => (int) $valute->Nominal,
-                    'currency_name' => (string) $valute->Name,
+  $attribs = $simple_xml->attributes();
+  $valute_array = $simple_xml->children();
+  var_dump($attribs);
+  foreach ($valute_array as $valute) {
+    $attr = $valute->attributes();
+    $valute_id = (string) $attr['ID'];
+    $currency_record = db_merge('bnm_currency')
+      ->key(array(
+        'valute_id' => (int) $valute_id,
+        'lang' => $lang,
+      ))
+      ->fields(
+        array(
+          'num_code' => (string) $valute->NumCode,
+          'char_code' => (string) $valute->CharCode,
+          'nominal' => (int) $valute->Nominal,
+          'currency_name' => (string) $valute->Name,
 //                    'lang' => $lang,
-                ))
-            ->execute();
+        ))
+      ->execute();
 
-        $rates_record = db_merge('bnm_exchange_rate')
-            ->key(array(
-                'valute_id' => (int) $valute_id,
-                'date' => (string) $attribs['Date'],
-            ))
-            ->fields(array(
-                'value' => (float) $valute->Value
-            ))
-            ->execute();
-    }
-
+    $rates_record = db_merge('bnm_exchange_rate')
+      ->key(array(
+        'valute_id' => (int) $valute_id,
+        'date' => (string) $attribs['Date'],
+      ))
+      ->fields(array(
+        'value' => (float) $valute->Value
+      ))
+      ->execute();
+  }
 }
 
+/**
+ * Select exchange rates from database
+ * If there is no data in the database,
+ * then call function bnm_rates_pull_xmldata($date = '', $lang = 'en')
+ * if there is no result from bnm_rates-pull-xmldata then log this event into watchdog
+ * @param string $date
+ * @param string $lang
+ */
 function bnm_rates_select($date = '', $lang = 'en') {
+
 
 }
